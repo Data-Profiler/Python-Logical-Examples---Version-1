df126b5b4e2f64a6aacd95a3427b7b8e38912b28
Padmaja D <99339931+Data-Profiler@users.noreply.github.com>
2022-02-15 22:25:46 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,15 @@
+class Student:
+	def __init__(self):
+		self.name="padmaja"
+		self.age=20
+		self.marks=900
+
+def talk(self):
+    print("hi,i am", self.name)
+    print("my age is", self.age)
+    print("my marks are",self.marks)
+
+s1 = Student()
+print(s1.name)
+print(s1.age)
+
